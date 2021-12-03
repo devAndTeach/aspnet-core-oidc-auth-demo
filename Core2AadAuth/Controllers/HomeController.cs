f04b4ae123fac24e965728365dcad5880f51ff44
devandteach <devandteach-ccj@yahoo.com>
2021-12-03 17:45:04 -0500

    Initial Commit

---
+++ Core2AadAuth/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Core2AadAuth.Options;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Newtonsoft.Json;
+
+namespace Core2AadAuth.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+       
+
+        public HomeController()
+        {            
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Index() => View();
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public IActionResult ProtectedResource() => View();
+
+       
+    }
+}
---
+++ Core2AadAuth/Startup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Core2AadAuth.Options;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace Core2AadAuth
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+            .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDataProtection();
+
+            //Add a strongly-typed options class to DI
+            services.Configure<AuthOptions>(Configuration.GetSection("Authentication"));
+
+           
+            services.AddAuthentication(auth =>
+            {
+                auth.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                auth.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddCookie()
+            .AddOpenIdConnect( opts => {
+                Configuration.GetSection("Authentication").Bind(opts);
+            });
+            
+        
+
+            services.Configure<HstsOptions>(o =>
+            {
+                o.IncludeSubDomains = false;
+                o.Preload = false;
+                o.MaxAge = TimeSpan.FromDays(365);
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                //Outside dev, require HTTPS and use HSTS
+                app.UseHttpsRedirection();
+                app.UseHsts();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
